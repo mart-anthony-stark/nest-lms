aa8af5499faf1aff79737a2226eb478a4eef35b6
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-27 18:48:43 +0800

    add AdminModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AdminController } from './admin/admin.controller';
 import { AdminService } from './admin/admin.service';
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
