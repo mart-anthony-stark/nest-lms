fd57b15f392b4ff6f776d5a38e28e85a73b1f36d
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-27 20:19:56 +0800

    add timestamps

--- server/src/schemas/student.schema.ts
+++ server/src/schemas/student.schema.ts
@@ -1,23 +1,30 @@
 import * as mongoose from 'mongoose';
 
-export const StudentSchema = new mongoose.Schema({
-  firstname: {
-    type: String,
-    required: [true, 'Student firstname is required'],
+export const StudentSchema = new mongoose.Schema(
+  {
+    firstname: {
+      type: String,
+      required: [true, 'Student firstname is required'],
+    },
+    middlename: {
+      type: String,
+      required: [true, 'Student middlename is required'],
+    },
+    lastname: {
+      type: String,
+      required: [true, 'Student lastname is required'],
+    },
+    dob: { type: Date },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'Email is required'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: [8, 'Password must be at least 8 characters'],
+    },
   },
-  middlename: {
-    type: String,
-    required: [true, 'Student middlename is required'],
-  },
-  lastname: { type: String, required: [true, 'Student lastname is required'] },
-  email: {
-    type: String,
-    unique: true,
-    required: [true, 'Email is required'],
-  },
-  password: {
-    type: String,
-    required: [true, 'Password is required'],
-    minlength: [8, 'Password must be at least 8 characters'],
-  },
-});
+  { timestamps: true },
+);
