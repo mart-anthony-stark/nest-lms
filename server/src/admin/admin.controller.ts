20d2dd17ffa0c0a18a49950f606a0378f9799ec3
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-27 19:20:33 +0800

    add registerAdmin

--- server/src/admin/admin.controller.ts
+++ server/src/admin/admin.controller.ts
@@ -1,6 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AdminService } from './admin.service';
+import { CreateAdminDTO } from './dto/createAdminDTO';
 
 @Controller('admin')
 export class AdminController {
-    
+  constructor(private readonly adminService: AdminService) {}
+
+  @Post('/register')
+  registerAdmin(@Body() createAdminDto: CreateAdminDTO) {
+    return this.adminService.registerAdmin(createAdminDto);
+  }
 }
