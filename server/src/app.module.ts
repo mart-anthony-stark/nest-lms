fd2d4749418130305fd24cf61a6b8c5d2878440a
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-27 19:43:06 +0800

    remove student controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminController } from './admin/admin.controller';
-import { AdminService } from './admin/admin.service';
 import { AdminModule } from './admin/admin.module';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AdminModule, MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [
+    ConfigModule.forRoot(),
+    AdminModule,
+    StudentModule,
+    MongooseModule.forRoot(process.env.DB_URI),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
