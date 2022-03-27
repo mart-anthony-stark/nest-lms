bf462ec4d2c5ce3db3cb135539ed78d09d457b13
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-27 19:08:49 +0800

    add mongoose init

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,9 +5,10 @@ import { AdminController } from './admin/admin.controller';
 import { AdminService } from './admin/admin.service';
 import { AdminModule } from './admin/admin.module';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigModule.forRoot(), AdminModule],
+  imports: [ConfigModule.forRoot(), AdminModule, MongooseModule.forRoot(process.env.DB_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
