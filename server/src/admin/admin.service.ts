c15f6535e92103dbd768a3454db882133168516d
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-27 19:20:41 +0800

    add registerAdmin

--- server/src/admin/admin.service.ts
+++ server/src/admin/admin.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Admin } from './interfaces/admin.interface';
 
 @Injectable()
-export class AdminService {}
+export class AdminService {
+  constructor(
+    @InjectModel('Admin') private readonly adminModel: Model<Admin>,
+  ) {}
+
+  async registerAdmin(body: Admin): Promise<Admin> {
+    return await new this.adminModel(body).save();
+  }
+}
