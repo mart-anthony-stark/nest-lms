18794fb57a2751c7c483bfb4600807df3ffb7a6c
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-27 18:57:43 +0800

    add admin schema

---
+++ server/src/schemas/admin.schema.ts
@@ -0,0 +1,18 @@
+import * as mongoose from 'mongoose';
+
+export const AdminSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Name is required'],
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Email is required'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [8, 'Password must be at least 8 characters'],
+  },
+});
