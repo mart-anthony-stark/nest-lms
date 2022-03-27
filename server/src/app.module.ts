e2f6c543765fb97c0f41c2d917cef0f05433b6f6
mart-anthony-stark <martsalazar0@gmail.com>
2022-03-27 18:42:48 +0800

    add admin controller

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminController } from './admin/admin.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, AdminController],
   providers: [AppService],
 })
 export class AppModule {}
